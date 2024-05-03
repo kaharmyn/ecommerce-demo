2cb0f83f70c1a4218696fe569d5e3fb56e905347
ubuntu-pc <akhayev.a@protonmail.com>
2024-05-03 16:21:38 +0500

    Final commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 # flake8: off
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 import os
 from os.path import join, dirname
@@ -148,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
